29e0f7c131f6f223614080a830babfb132643abd
sijin111 <sijin5231@naver.com>
2024-11-25 11:52:36 +0900

    feat : 최신등록게임 목록표시 기능추가
    최신등록게임 목록표시 기능 추가

--- src/main/java/game/model/GameDAO_imple.java
+++ src/main/java/game/model/GameDAO_imple.java
@@ -75,8 +75,47 @@ public class GameDAO_imple implements GameDAO {
 
 	@Override
 	public List<GameDTO> getRecentGames() {
-		// TODO Auto-generated method stub
-		return null;
+		List<GameDTO> Recent_gameList = new ArrayList<>();
+		
+		try {
+			String sql = " select pk_game_no, title, intro, image, link, player_cnt, passwd, c.name as category_name "
+					   + " from TBL_GAME G join TBL_GAME_CATEGORY GC "
+					   + " on G.pk_game_no = GC.fk_game_no "
+					   + " join tbl_category C "
+					   + " on GC.fk_category_no = C.pk_category_no"
+					   + " where rownum <= 4 "
+					   + " order by register_day desc";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery(); // sql문 실행
+			
+			while(rs.next()) {
+				
+				GameDTO gameDTO = new GameDTO();
+				
+				gameDTO.setPkGameNo(rs.getInt("pk_game_no"));
+				gameDTO.setTitle(rs.getString("title"));
+				gameDTO.setIntro(rs.getString("intro"));
+				gameDTO.setImage(rs.getString("image"));
+				gameDTO.setLink(rs.getString("link"));
+//				gameDTO.setIsDelete(rs.getInt("is_delete"));
+//				gameDTO.setViews(rs.getInt("views"));
+//				gameDTO.setRegisterDay(rs.getString("register_day"));
+				gameDTO.setPlayerCnt(rs.getInt("player_cnt"));
+				gameDTO.setPasswd(rs.getString("passwd"));
+				gameDTO.setCategoryName(rs.getString("category_name"));
+				
+				Recent_gameList.add(gameDTO);
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return Recent_gameList;
 	}
 
 	/*
